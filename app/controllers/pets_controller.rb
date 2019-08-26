d57cff5c3a45874a5f7c97c51f775531ef8732a8
Audrey Garcia <audrey.marcella9@gmail.com>
2019-08-26 16:45:15 -0600

    pet model, controller, crud

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,48 @@
+class PetsController < ApplicationController
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pets = current_user.pets
+  end
+
+  def show
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = current_user.pets.new(pet_params)
+    if @pet.save
+      redirect_to pets_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @pet.update(pet_params)
+      redirect_to pet_path(@pet)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @pet.destroy
+    redirect_to pets_path
+  end
+
+  private
+    def pet_params
+      params.require(:pet).permit(:name, :species, :age)
+    end
+
+    def set_pet
+      @pet = current_user.pets.find(params[:id])
+    end
+end
---
+++ db/migrate/20190826195043_create_pets.rb
@@ -0,0 +1,12 @@
+class CreatePets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :species
+      t.integer :age
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
